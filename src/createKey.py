a4ad35d7f5489e59f317feb4f9c9ba732ed716be
Diógenes Ribeiro de Oliveira <91908375+diogenesOliver@users.noreply.github.com>
2023-05-24 19:24:06 +0000

    feat: Function to Create a key created

--- src/createKey.py
+++ src/createKey.py
@@ -1,3 +1,6 @@
-def CreateKey(key):
+def create_key(key):
     while len(key) < 10:
         print('Insufficient key size... Key size must be greater than ten')
+        key = input('Enter a key: ')
+
+    return key
